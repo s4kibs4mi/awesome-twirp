ba8b10ccc5ce0a738d7c63614f360e2e1834489b
Sakib Sami <s4kibs4mi@gmail.com>
2018-06-01 01:07:00 +0600

    Update : Code refactoring

--- generate_proto.sh
+++ generate_proto.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 export GOPATH=$HOME/go/
-protoc --proto_path=$GOPATH/src:. --twirp_out=. --go_out=. ./protobuf/pdefs/*.proto
+protoc --proto_path=$GOPATH/src:. --twirp_out=./../../../ --go_out=./../../../ ./protobuf/pdefs/*.proto
